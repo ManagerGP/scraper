2e126f59709e8d8c2e1ae75f5db0c30731b89eed
lorenzokyne <l.capocchiano@studenti.uniba.it>
2021-04-13 19:46:19 +0200

    Added scraper service + scraper code refactoring

--- Controllers/ScraperController.cs
+++ Controllers/ScraperController.cs
@@ -1,21 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
-using HtmlAgilityPack;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using Newtonsoft.Json.Linq;
+using BahrainGp.Services;
 
-namespace ArticoliWebService.AddControllers{
-/// <summary>
-/// This controller allows you to extrapolate information on the MTGP, FE and F1 
-/// </summary>
+namespace ArticoliWebService.AddControllers
+{
+    /// <summary>
+    /// This controller allows you to extrapolate information on the MTGP, FE and F1 
+    /// </summary>
     [ApiController]
     [Route("scraper")]
-    public class ScraperController: ControllerBase
-    {      
+    public class BahrainGpController : ControllerBase
+    {
         /// <summary>
-        /// This method allows you to extrapolet information about riders tables
+        /// This method allows you to extrapolate information about riders tables
         /// url : Url to receive table information
         /// xpath: Indicates the name of the table
         /// 
@@ -25,51 +21,11 @@ namespace ArticoliWebService.AddControllers{
         /// <param name="xpath"></param>
         /// <returns>result</returns>
         [HttpPost("table")]
-         public IActionResult GetTableInformation([FromForm] string url, [FromForm] string xpath) {
-            string result ="";
+        public IActionResult GetTableInformation([FromForm] string url, [FromForm] string xpath)
+        {
+            string result = "";
+            result = ScraperService.PerformScraping(url, xpath);
 
-            using (HttpClient client = new HttpClient())
-            {
-                using (HttpResponseMessage response = client.GetAsync(url).Result)
-                {
-                    using (HttpContent content = response.Content)
-                    {
-                        result = content.ReadAsStringAsync().Result;
-                    }
-                }                         
-            }
-            
-            HtmlDocument document = new HtmlDocument();
-            document.LoadHtml(result);
-            
-            var web = new HtmlWeb();
-            int z = 1;
-            var tableElem = document.DocumentNode.SelectSingleNode(xpath);
-            List<string> header = tableElem.Descendants("tr")
-                        .Take(1)
-                        .Where(tr => tr.Elements("th").Count() > 1)
-                        .SelectMany(tr => tr.Elements("th").Select(td => td.InnerText.Trim()).ToList())
-                        .ToList().Select(elem => elem == string.Empty ? (z++).ToString(): elem).ToList();  
-
-            List<List<string>> table = tableElem
-                        .Descendants("tr")
-                        .Skip(1)
-                        .Where(tr => tr.Elements("td").Count() > 1)
-                        .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
-                        .ToList();
-            result = string.Empty;
-            JArray json = new JArray();
-            foreach (var row in table)
-            {
-                JObject elementRow = new JObject();
-                int i = 0;
-                foreach (var cell in row)
-                {
-                    elementRow.Add(header[i++], new JValue(Regex.Replace(cell, @"\n\s+", " ")));       
-                }
-                json.Add(elementRow);
-            }
-            result=json.ToString();                 
             return Ok(result);
         }
     }
---
+++ Services/Scraper.cs
@@ -0,0 +1,80 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace BahrainGp.Services
+{
+    public static class ScraperService
+    {
+        public static String PerformScraping(string url, string xpath)
+        {
+            string result = string.Empty;
+
+            result = GetHtmlPage(url);
+            List<string> header = new List<string>();
+            List<List<string>> table = new List<List<string>>();
+
+            SetTableData(result, xpath, ref header, ref table);
+            result = string.Empty;
+            result = GetJsonStringData(header, table);
+            return result;
+        }
+
+        private static string GetHtmlPage(string url)
+        {
+            string result = string.Empty;
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        result = content.ReadAsStringAsync().Result;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static void SetTableData(string result, string xpath, ref List<string> header, ref List<List<string>> table)
+        {
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(result);
+            int z = 1;
+            var tableElem = document.DocumentNode.SelectSingleNode(xpath);
+            header = tableElem.Descendants("tr")
+                        .Take(1)
+                        .Where(tr => tr.Elements("th").Count() > 1)
+                        .SelectMany(tr => tr.Elements("th").Select(td => td.InnerText.Trim()).ToList())
+                        .ToList().Select(elem => elem == string.Empty ? (z++).ToString() : elem).ToList();
+
+            table = tableElem
+                        .Descendants("tr")
+                        .Skip(1)
+                        .Where(tr => tr.Elements("td").Count() > 1)
+                        .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                        .ToList();
+        }
+
+        private static string GetJsonStringData(List<string> header, List<List<string>> table)
+        {
+            JArray json = new JArray();
+            foreach (var row in table)
+            {
+                JObject elementRow = new JObject();
+                int i = 0;
+                foreach (var cell in row)
+                {
+                    elementRow.Add(header[i++], new JValue(Regex.Replace(cell, @"\n\s+", " ")));
+                }
+                json.Add(elementRow);
+            }
+            return json.ToString();
+        }
+    }
+}
