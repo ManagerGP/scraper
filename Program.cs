26c0ef9290e277b6cfc8574f2a01ad26c520c18e
lorenzokyne <l.capocchiano@studenti.uniba.it>
2021-04-14 19:40:31 +0200

    Fix missing changes

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Scraper
+namespace BahrainGp
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Scraper
+namespace BahrainGp
 {
     public class Startup
     {
